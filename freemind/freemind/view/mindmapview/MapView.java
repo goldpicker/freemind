d476bb9ace3ec32d3d125a280730a5acae4a9923
dpolivaev <dpolivaev>
2004-10-13 20:54:51 +0000

    CR / LF corrected

--- freemind/freemind/view/mindmapview/MapView.java
+++ freemind/freemind/view/mindmapview/MapView.java
@@ -16,7 +16,7 @@
  *along with this program; if not, write to the Free Software
  *Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-/*$Id: MapView.java,v 1.30 2004-01-25 16:41:26 christianfoltin Exp $*/
+/*$Id: MapView.java,v 1.30.14.1 2004-10-13 20:54:51 dpolivaev Exp $*/
 
 package freemind.view.mindmapview;
 
