8fe1a1fb62adb631de22dec99ab3940106b76153
christianfoltin <christianfoltin>
2008-01-30 20:52:58 +0000

    new screenshots, start of doc map.

---
+++ admin/docs/features/0_9_0/Features 0.9.0.mm
@@ -0,0 +1,439 @@
+<map version="0.9.0_Beta_8">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1201282183606" ID="Freemind_Link_1027666650" MODIFIED="1201716973969" STYLE="fork" TEXT="Features 0.9.0">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<attribute NAME="script1" VALUE="def stack = new java.util.Stack();&#xa;stack.push(node);&#xa;while(stack.size()&gt;0) {&#xa;&#x9;def current =stack.pop();&#xa;&#x9;print current.getShortText(c) + &quot;, &quot;;&#xa;&#x9;stack.addAll(current.getChildren());&#xa;}"/>
+<node COLOR="#0033ff" CREATED="1201283648829" ID="Freemind_Link_813308322" MODIFIED="1201283659663" POSITION="right" STYLE="fork" TEXT="Main new features">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1201283660674" FOLDED="true" ID="Freemind_Link_1390621915" MODIFIED="1201283805485" STYLE="fork" TEXT="New Editor">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The new editor supports text formatting in nodes and notes (the little window at the bottom of the frame).
+    </p>
+    <p>
+      It is stored as (X)HTML inside the nodes and can be thus exported to HTML very efficiently.
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1201283668056" ID="Freemind_Link_433683362" MODIFIED="1201283681903">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="node_editor_simplyhtml.png" />
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1201283685546" ID="Freemind_Link_631373506" MODIFIED="1201283691657">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="node_editor_simplyhtml_html_view.png" />
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1201283806888" FOLDED="true" ID="Freemind_Link_1546698647" MODIFIED="1201283938618" STYLE="fork" TEXT="Notes enhancements">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The notes do now directly belong to the main window. Even if it is not visible a tooltip indicates the content.
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1201283813962" ID="Freemind_Link_1086164438" MODIFIED="1201283819446">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="note_window.png" />
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1201506412267" ID="Freemind_Link_233524870" MODIFIED="1201506443515">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="note_window_tooltip.png" />
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1201506800600" ID="Freemind_Link_9416334" MODIFIED="1201506802120" STYLE="fork" TEXT="Filter">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1201506806036" ID="Freemind_Link_1096073845" MODIFIED="1201506808274" STYLE="fork" TEXT="Attributes">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1201282217311" ID="Freemind_Link_849247054" MODIFIED="1201282223659" POSITION="right" STYLE="fork" TEXT="Visual Changes">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1201282224869" FOLDED="true" ID="Freemind_Link_1307834035" MODIFIED="1201282277049" STYLE="fork" TEXT="Tabular MindMapping Support">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      It is possible to see all currently open maps at a glance.
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1201282281946" ID="Freemind_Link_1864289916" MODIFIED="1201282300067">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="tabular_support.png" />
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1201282406968" FOLDED="true" ID="Freemind_Link_101409050" MODIFIED="1201282414121" STYLE="fork" TEXT="Icon Toolbar is scrollable">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1201282415646" ID="Freemind_Link_695696608" MODIFIED="1201282422569">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="icon_toolbar_scrollable.png" />
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1201282319118" ID="Freemind_Link_1043646949" MODIFIED="1201282324582" POSITION="right" STYLE="fork" TEXT="Usability Improvements">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1201282325640" ID="Freemind_Link_556186325" MODIFIED="1201282331416" STYLE="fork" TEXT="Move in all directions">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1201282465994" FOLDED="true" ID="Freemind_Link_924709284" MODIFIED="1201282711721" TEXT="Up and Down">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This kind of navigation was already published in 0.8.0. But now, you can use other directions as well.
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1201282472052" ID="Freemind_Link_917772022" MODIFIED="1201282475868">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="move_nodes_up_example.png" />
+  </body>
+</html></richcontent>
+</node>
+<node COLOR="#111111" CREATED="1201282476870" ID="Freemind_Link_495589866" MODIFIED="1201282479045">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="move_nodes_up_example1.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1201282426565" ID="Freemind_Link_481786702" MODIFIED="1201282843358" TEXT="Left and Right">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      It is now possible to move nodes left and right. They come closer to root or move as new subnodes of the node above. The nodes can even change the side of root with this navigation. You can use it by the control+left and control+right keys.
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1201282851749" FOLDED="true" ID="Freemind_Link_774463025" MODIFIED="1201282871361" TEXT="Getting higher and lower in the tree">
+<node COLOR="#111111" CREATED="1201282873949" ID="Freemind_Link_623953260" MODIFIED="1201282894121">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="move_nodes_right_example2.png" />
+  </body>
+</html></richcontent>
+</node>
+<node COLOR="#111111" CREATED="1201282895687" ID="Freemind_Link_1563328606" MODIFIED="1201282900505">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="move_nodes_right_example3.png" />
+  </body>
+</html></richcontent>
+</node>
+<node COLOR="#111111" CREATED="1201282902044" ID="Freemind_Link_1015311948" MODIFIED="1201282909323">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="move_nodes_right_example5.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1201282843249" FOLDED="true" ID="Freemind_Link_1554785271" MODIFIED="1201282850364" TEXT="Across the root node">
+<node COLOR="#111111" CREATED="1201282434688" ID="Freemind_Link_1728934284" MODIFIED="1201282440387">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="move_nodes_right_example.png" />
+  </body>
+</html></richcontent>
+</node>
+<node COLOR="#111111" CREATED="1201282441796" ID="Freemind_Link_221157986" MODIFIED="1201282446533">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="move_nodes_right_example1.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1201282332144" FOLDED="true" ID="Freemind_Link_536412761" MODIFIED="1201282340692" STYLE="fork" TEXT="Menu items have mnemonics now">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1201282344313" ID="Freemind_Link_1686909373" MODIFIED="1201282380292" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="menu_mnemonics_support.png" />
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1201282939150" ID="Freemind_Link_150258579" MODIFIED="1201282942471" POSITION="right" STYLE="fork" TEXT="New Exports">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1201282945124" FOLDED="true" ID="Freemind_Link_1356291624" MODIFIED="1201283459618" STYLE="fork" TEXT="As flash and as a Java applet">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Flash and Java applet export can be used to bring mindmaps into the internet.
+    </p>
+    <p>
+      The difference between the two is, that the Java applet looks exactly like the map on the screen, but needs longer to be loaded.
+    </p>
+    <p>
+      The flash application is very fast and has some different features built in. But it has a different look and feel.
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1201283573013" ID="Freemind_Link_1099251335" MODIFIED="1201283579454">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="new_exports.png" />
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1201282973565" ID="Freemind_Link_1360729329" MODIFIED="1201283563783">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="new_export_flash.png" />
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1201506466515" ID="Freemind_Link_676471791" MODIFIED="1201541941214" POSITION="right" STYLE="fork" TEXT="Scripting Support">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      FreeMind can now be scripted by using Groovy scripts. Groovy is a very easy to use scripting language best integrating into FreeMind.
+    </p>
+    <p>
+      There are two possibilities to use scripts:
+    </p>
+    <ol>
+      <li>
+        Choose the script editor to easily add, change and test your scripts. Technically, scripts are attached to a node via an attribute starting with &quot;script&quot; (like &quot;script1&quot;) that contains the script. Every time you choose &quot;Evaluate&quot; from the tools menu, all scripts attached to nodes are executed.
+      </li>
+      <li>
+        Create or change a pattern and press the script button. The script editor appears and your script will be associated to a pattern. Every time you apply the pattern to some nodes, the script is executed for that nodes automatically. Thus, you can have the scripts with keyboard shortcuts as the patterns are accessible via shortcuts.
+      </li>
+    </ol>
+    <p>
+      Every script is at evaluation time started with two predefined java objects coming from the map:
+    </p>
+    <ul>
+      <li>
+        node is the current node. It is a freemind.modes.MindMapNode. This node can be used to retrieve information about the contents, its children or its formatting. <i>Don't use the setter to change the node. Use the following instead:</i>
+      </li>
+      <li>
+        c is the current controller. It is a freemind.modes.mindmapmode.MindMapController. This controller <b>should</b> be used to change information. For example, if you want to change the nodes text or if you want to add children. The methods that can be used are sumarized in freemind.modes.mindmapmode.actions.MindMapActions.
+      </li>
+    </ul>
+    <p>
+      There are two automatisms regarding the effect of a script:
+    </p>
+    <ol>
+      <li>
+        If a script starts with &quot;=&quot;, the result of the script is taken to be the new nodes text. Example script1: =17+4. If executed, the node the script is associated to will be changed to 21.
+      </li>
+      <li>
+        If a script starts with letters only and then a &quot;=&quot; sign, like &quot;sum=17+4&quot;, the result is taken to be a (possibly new) attribute named &quot;sum&quot; in this case with the content 21.
+      </li>
+    </ol>
+    <p>
+      For more examples consult our little scripting guide below or our web pages.
+    </p>
+    <p>
+      A last word on security: before scripts is evaluated for the first time in FreeMind, the user is asked whether or not he allows it. The answer can be stored for every script but observe that a malicious script is able to perform every action on your computer that your users rights allow up to delete all files or send them to pirates.ru. This said, be careful and don't allow scripts when you don't know that the author is trusted. Finally, scripts are never evaluated automatically in FreeMind for these reasons. Thus, you can open a map without problems and have a look at the scripts it contains.
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1201506526029" ID="Freemind_Link_1885521461" MODIFIED="1201506532746" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="script_editor.png" />
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1201506546021" ID="Freemind_Link_1289478680" MODIFIED="1201506552477" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="script_editor_attached_script.png" />
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1201506596957" ID="Freemind_Link_1136959722" MODIFIED="1201506612855" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="script_example_script_editor.png" />
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1201506491455" ID="Freemind_Link_1324027157" MODIFIED="1201506510094" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="script_example_mm.png" />
+  </body>
+</html></richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1201506580407" ID="Freemind_Link_1055396713" MODIFIED="1201717092414" STYLE="fork" TEXT="Little FreeMind Scripting Guide">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Here we present some snippets of useful Groovy code that can be used as parts of your scripts. More scripts can be found on our web sides.
+    </p>
+    <ul>
+      <li>
+        Read an attribute<br />def value = node.getAttribute(&quot;key&quot;); // value is of type String.<br /><br />
+      </li>
+      <li>
+        Create or change attributes: the following method checks whether or not an attribute with the same key exists and replaces it. Otherwise a new attribute is created and added.<br />c.editAttribute(node, &quot;key&quot;, &quot;new value&quot;);<br />
+      </li>
+      <li>
+        Traverse all children<br />def it = node.childrenUnfolded();<br />while(it.hasNext()) {<br />def child = i.next();<br />}
+      </li>
+      <li>
+        Traverse all children and its children recursively. The following examples prints the content of every node including its childs<br />def stack = new java.util.Stack();<br />stack.push(node);<br />while(stack.size()&gt;0) {<br />def current =stack.pop();<br />print current.getShortText(c) + &quot;, &quot;;<br />stack.addAll(current.getChildren());<br />}<br /><br />
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
