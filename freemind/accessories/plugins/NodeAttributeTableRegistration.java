9ebd72de705fd1824c8dd80535b233e90c356ae9
Christian Foltin <christianfoltin@users.sourceforge.net>
2014-09-04 17:45:08 +0200

    * Raw attribute table added.

--- freemind/accessories/plugins/NodeAttributeTableRegistration.java
+++ freemind/accessories/plugins/NodeAttributeTableRegistration.java
@@ -24,13 +24,19 @@
 package accessories.plugins;
 
 import java.awt.event.ActionEvent;
+import java.util.Vector;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
-import javax.swing.JLabel;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.AbstractTableModel;
 
+import accessories.plugins.time.TableSorter;
+import freemind.common.TextTranslator;
 import freemind.controller.Controller.SplitComponentType;
 import freemind.controller.MenuItemSelectedListener;
 import freemind.extensions.HookRegistration;
@@ -43,6 +49,120 @@ import freemind.modes.mindmapmode.MindMapController;
 public class NodeAttributeTableRegistration implements HookRegistration, 
 		MenuItemSelectedListener {
 
+	public final class AttributeHolder {
+		public String mKey;
+		public String mValue;
+	}
+	
+	private static final int KEY_COLUMN = 0;
+
+	public static final int VALUE_COLUMN = 1;
+
+	private static final String KEY_COLUMN_TEXT = "plugins/map/MapDialog.Description";
+
+	private static final String VALUE_COLUMN_TEXT = "plugins/map/MapDialog.Distance";
+
+	/**
+	 * @author foltin
+	 * @date 4.09.2014
+	 */
+	public final class AttributeTableModel extends AbstractTableModel  {
+		/**
+		 * 
+		 */
+		private final String[] COLUMNS = new String[] {
+				KEY_COLUMN_TEXT, VALUE_COLUMN_TEXT };
+		Vector<AttributeHolder> mData = new Vector<AttributeHolder>();
+		private final TextTranslator mTextTranslator;
+
+		/**
+		 */
+		public AttributeTableModel(TextTranslator pTextTranslator) {
+			super();
+			mTextTranslator = pTextTranslator;
+		}
+
+		/**
+		 * @param pAttribute
+		 */
+		public void addAttributeHolder(AttributeHolder pAttribute) {
+			mData.add(pAttribute);
+			final int row = mData.size() - 1;
+			fireTableRowsInserted(row, row);
+		}
+		
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see javax.swing.table.AbstractTableModel#getColumnClass(int)
+		 */
+		public Class getColumnClass(int arg0) {
+			switch (arg0) {
+			case KEY_COLUMN:
+			case VALUE_COLUMN:
+				return String.class;
+			default:
+				return Object.class;
+			}
+		}
+
+		public AttributeHolder getAttributeHolder(int pIndex) {
+			return mData.get(pIndex);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see javax.swing.table.AbstractTableModel#getColumnName(int)
+		 */
+		public String getColumnName(int pColumn) {
+			return mTextTranslator.getText(COLUMNS[pColumn]);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see javax.swing.table.TableModel#getRowCount()
+		 */
+		public int getRowCount() {
+			return mData.size();
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see javax.swing.table.TableModel#getColumnCount()
+		 */
+		public int getColumnCount() {
+			return 2;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see javax.swing.table.TableModel#getValueAt(int, int)
+		 */
+		public Object getValueAt(int pRowIndex, int pColumnIndex) {
+			final AttributeHolder attr = getAttributeHolder(pRowIndex);
+			switch (pColumnIndex) {
+			case KEY_COLUMN:
+				return attr.mKey;
+			case VALUE_COLUMN:
+				return attr.mValue;
+			}
+			return null;
+		}
+
+		/**
+		 * 
+		 */
+		public void clear() {
+			mData.clear();
+			fireTableDataChanged();
+		}
+
+	}
+	
 	private final MindMapController controller;
 
 	private final java.util.logging.Logger logger;
@@ -50,6 +170,12 @@ public class NodeAttributeTableRegistration implements HookRegistration,
 	private boolean mSplitPaneVisible = false;
 	
 	private JPanel mAttributeViewerComponent = null;
+
+	private JTable mAttributeTable;
+
+	private AttributeTableModel mAttributeTableModel;
+
+	private TableSorter mAttributeTableSorter;
 
 	public NodeAttributeTableRegistration(ModeController controller, MindMap map) {
 		this.controller = (MindMapController) controller;
@@ -77,7 +203,19 @@ public class NodeAttributeTableRegistration implements HookRegistration,
 
 	public void register() {
 		mAttributeViewerComponent = new JPanel();
-		mAttributeViewerComponent.add(new JLabel("Tabelle!"));
+		mAttributeTable = new JTable();
+		mAttributeTable.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		mAttributeTable.getTableHeader().setReorderingAllowed(false);
+		mAttributeTableModel = new AttributeTableModel(controller);
+		mAttributeTableSorter = new TableSorter(mAttributeTableModel);
+		mAttributeTable.setModel(mAttributeTableSorter);
+		mAttributeTableSorter.setTableHeader(mAttributeTable.getTableHeader());
+		mAttributeTableSorter.setColumnComparator(String.class,
+				TableSorter.LEXICAL_COMPARATOR);
+		// Sort by default by date.
+		mAttributeTableSorter.setSortingStatus(KEY_COLUMN,
+				TableSorter.ASCENDING);
+		mAttributeViewerComponent.add(new JScrollPane(mAttributeTable));
 		// register "leave note" action:
 		if (shouldShowSplitPane()) {
 			showAttributeTablePanel();
